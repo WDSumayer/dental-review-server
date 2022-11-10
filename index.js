b71ac528aa40ef157d83ac90fb33d5d159ed47e2
unknown <sumairbinkalam20@gmail.com>
2022-11-10 14:36:36 +0600

    token created

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors')
+const jwt = require('jsonwebtoken')
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 
